ef845d0ac7b1a960ff743950401fbb4acadce7b7
Illia Komarskyi <102596775+ottakist@users.noreply.github.com>
2025-01-22 20:12:37 +0200

    project configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  mode: 'jit',
   theme: {
-    extend: {},
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+      kyiv: ['KyivType', 'serif']
+    },
+    extend: {}
   },
   plugins: [],
+  safelist: ['bg-red-500', 'text-xl']
 }
-
